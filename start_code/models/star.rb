af59d544f305543b144b9f92a1fce59cd407b1dc
Alina <lindenalina.al@gmail.com>
2020-07-02 16:04:01 +0100

    display all movies of a star passed

--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -37,4 +37,13 @@ class Star
         SqlRunner.run(sql)
     end
 
+    def movies()
+        sql = "SELECT movies.* FROM movies
+        INNER JOIN castings ON
+        castings.movie_id = movies.id WHERE star_id = $1"
+        values = [@id]
+        movies = SqlRunner.run(sql, values)
+        return movies.map { |movie| Movie.new(movie) }
+    end
+
 end
